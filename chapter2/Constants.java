aca80613d3e9cc6b5a2a40bfb070ad9644994d1e
pacosonTang <pacoson@163.com>
2016-04-07 15:02:25 +0800

    test for a simple servlet container

---
+++ chapter2/Constants.java
@@ -0,0 +1,8 @@
+package com.tomcat.chapter2;
+
+import java.io.File;
+
+public class Constants {
+  public static final String WEB_ROOT =
+     System.getProperty("user.dir") + File.separator  + "webroot";	
+}
