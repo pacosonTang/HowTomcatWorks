63f1bfba7f221dedf28a8f9eb36a3c819f6f4c22
pacosonTang <pacoson@163.com>
2016-04-09 20:38:51 +0800

    web resource

---
+++ webroot/servlet/Bean.java
@@ -0,0 +1,10 @@
+package servlet;
+
+public class Bean {
+	public void sayHello() {
+		System.out.println("Hello from loaded Bean class for servlet package!!!");
+	}
+	public static void main(String[] args) {
+		System.out.println("i am a bean!");
+	}
+}
