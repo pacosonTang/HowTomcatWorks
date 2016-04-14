7f2f9fe1db89bb06e6eaee6bb5cf60f95f3602bb
pacosonTang <pacoson@163.com>
2016-04-14 23:00:10 +0800

    test for realizing Logger

---
+++ chapter7/startup/Bootstrap.java
@@ -0,0 +1,69 @@
+package com.tomcat.chapter7.startup;
+
+import org.apache.catalina.Connector;
+import org.apache.catalina.Context;
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleListener;
+import org.apache.catalina.Loader;
+import org.apache.catalina.logger.FileLogger;
+import org.apache.catalina.Mapper;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.connector.http.HttpConnector;
+
+import com.tomcat.chapter7.core.SimpleContext;
+import com.tomcat.chapter7.core.SimpleContextLifecycleListener;
+import com.tomcat.chapter7.core.SimpleContextMapper;
+import com.tomcat.chapter7.core.SimpleLoader;
+import com.tomcat.chapter7.core.SimpleWrapper;
+
+public final class Bootstrap {
+  public static void main(String[] args) {
+    Connector connector = new HttpConnector();
+    Wrapper wrapper1 = new SimpleWrapper();
+    wrapper1.setName("Primitive");
+    wrapper1.setServletClass("PrimitiveServlet");
+    Wrapper wrapper2 = new SimpleWrapper();
+    wrapper2.setName("Modern");
+    wrapper2.setServletClass("ModernServlet");
+    Loader loader = new SimpleLoader();
+
+    Context context = new SimpleContext();
+    context.addChild(wrapper1);
+    context.addChild(wrapper2);
+
+    Mapper mapper = new SimpleContextMapper();
+    mapper.setProtocol("http");
+    LifecycleListener listener = new SimpleContextLifecycleListener();
+    ((Lifecycle) context).addLifecycleListener(listener);
+    context.addMapper(mapper);
+    context.setLoader(loader);
+    // context.addServletMapping(pattern, name);
+    context.addServletMapping("/Primitive", "Primitive");
+    context.addServletMapping("/Modern", "Modern");
+
+    // ------ add logger -------- highlight
+    System.setProperty("catalina.base", System.getProperty("user.dir"));
+    FileLogger logger = new FileLogger();
+    logger.setPrefix("FileLog_");
+    logger.setSuffix(".txt");
+    logger.setTimestamp(true);
+    logger.setDirectory("webroot");
+    context.setLogger(logger);
+
+    //--------------------------- highlight over.
+
+    connector.setContainer(context);
+    try {
+      connector.initialize();
+      ((Lifecycle) connector).start();
+      ((Lifecycle) context).start();
+
+      // make the application wait until we press a key.
+      System.in.read();
+      ((Lifecycle) context).stop();
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
