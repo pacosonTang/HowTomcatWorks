66536ecaab0806a9855dfc3e5edd37a056c23a08
pacosonTang <pacoson@163.com>
2016-05-08 21:59:04 +0800

    test for tomcat security

---
+++ chapter10/startup/Bootstrap2.java
@@ -0,0 +1,94 @@
+package com.tomcat.chapter10.startup;
+
+import java.io.File;
+
+import org.apache.catalina.Connector;
+import org.apache.catalina.Context;
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleListener;
+import org.apache.catalina.Loader;
+import org.apache.catalina.Realm;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.connector.http.HttpConnector;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.deploy.LoginConfig;
+import org.apache.catalina.deploy.SecurityCollection;
+import org.apache.catalina.deploy.SecurityConstraint;
+import org.apache.catalina.loader.WebappLoader;
+
+import com.tomcat.chapter10.core.SimpleContextConfig;
+import com.tomcat.chapter10.core.SimpleWrapper;
+import com.tomcat.chapter10.realm.SimpleUserDatabaseRealm;
+
+public final class Bootstrap2 {
+  public static void main(String[] args) {
+
+  //invoke: http://localhost:8080/Modern or  http://localhost:8080/Primitive
+
+    System.setProperty("catalina.base", System.getProperty("user.dir"));
+    Connector connector = new HttpConnector();
+    Wrapper wrapper1 = new SimpleWrapper();
+    wrapper1.setName("Primitive");
+    wrapper1.setServletClass("servlet.PrimitiveServlet");
+    Wrapper wrapper2 = new SimpleWrapper();
+    wrapper2.setName("Modern");
+    wrapper2.setServletClass("servlet.ModernServlet");
+
+    Context context = new StandardContext();
+    // StandardContext's start method adds a default mapper
+    context.setPath("/myApp");
+    context.setDocBase("myApp");
+    LifecycleListener listener = new SimpleContextConfig();
+    ((Lifecycle) context).addLifecycleListener(listener);
+
+    context.addChild(wrapper1);
+    context.addChild(wrapper2);
+    // for simplicity, we don't add a valve, but you can add
+    // valves to context or wrapper just as you did in Chapter 6
+
+    Loader loader = new WebappLoader();
+    context.setLoader(loader);
+    // context.addServletMapping(pattern, name);
+    context.addServletMapping("/Primitive", "Primitive");
+    context.addServletMapping("/Modern", "Modern");
+    // add ContextConfig. This listener is important because it configures
+    // StandardContext (sets configured to true), otherwise StandardContext
+    // won't start
+
+    // add constraint
+    SecurityCollection securityCollection = new SecurityCollection();
+    securityCollection.addPattern("/");
+    securityCollection.addMethod("GET");
+
+    SecurityConstraint constraint = new SecurityConstraint();
+    constraint.addCollection(securityCollection);
+    constraint.addAuthRole("manager");
+    constraint.addAuthRole("tomcat");
+    LoginConfig loginConfig = new LoginConfig();
+    loginConfig.setRealmName("Simple User Database Realm");
+    // add realm
+    Realm realm = new SimpleUserDatabaseRealm();
+    String filedir = new File(System.getProperty("user.dir")).getParent() + File.separator + 
+    		"conf" + File.separator + "tomcat-users.xml"; 
+    ((SimpleUserDatabaseRealm) realm).createDatabase(filedir);       
+    
+    context.setRealm(realm);
+    context.addConstraint(constraint);
+    context.setLoginConfig(loginConfig);
+
+    connector.setContainer(context);
+
+    try {
+      connector.initialize();
+      ((Lifecycle) connector).start();
+      ((Lifecycle) context).start();
+
+      // make the application wait until we press a key.
+      System.in.read();
+      ((Lifecycle) context).stop();
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
